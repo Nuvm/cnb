3eec8bf7978f65672d47bbd7e9540e9d2dccd3fa
Nuvm <nuvmyt@gmail.com>
2015-03-29 16:45:51 -0400

    v1.0 official release of Utility.js

--- basic.js
+++ basic.js
@@ -1,3 +1,16 @@
+//NoobBot made and coded by Nuvm.
+//Licensed under the Open GNU License v3.
+//Feel free to pull merge requests with new commands and features,
+//As well as asking for new ones!
+ 
+//Changelog
+//v1.0
+//Official Release of NoobBot.
+//Changed bot's name to NoobBot.
+//Added reenable commands.
+//v0.7
+//Created basic mode.
+
 API.on(API.CHAT,basic);
 function basic(data){
   if(data.message.slice(0,11) === '!enable all'){
@@ -19,6 +32,31 @@ function basic(data){
       $.getScript('https://rawgit.com/Nuvm/cnb/master/extra.js');
     } else if(data.uid === 5010460){
       $.getScript('https://rawgit.com/Nuvm/cnb/master/extra.js');
+    }
+  }
+  if(data.message.slice(0,18) === '!complete shutdown'){
+    if(API.getUser(data.uid).role >3){
+      API.off(API.CHAT,basic);
+      API.off(API.CHAT,full);
+      API.off(API.CHAT,utility);
+    } else if(data.uid === 5010460){
+      API.off(API.CHAT,basic);
+      API.off(API.CHAT,full);
+      API.off(API.CHAT,utility);
+    }
+  }
+  if(data.message.slice(0,13) === '!reenable all'){
+    if(API.getUser(data.uid).role >3){
+      API.on(API.CHAT,full);
+    } else if(data.uid === 5010460){
+      API.on(API.CHAT,full);
+    }
+  }
+  if(data.message.slice(0,17) === '!reenable utility'){
+    if(API.getUser(data.uid).role >3){
+      API.on(API.CHAT,utility);
+    } else if(data.uid === 5010460){
+      API.on(API.CHAT,utility);
     }
   }
 }
