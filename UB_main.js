cb468d9238da17cfa7d73a974a50a326f78a89a0
Nuvm <nuvmyt@gmail.com>
2015-05-05 18:41:36 -0400

    Update and rename basic.js to UB_main.js

---
+++ UB_main.js
@@ -0,0 +1,28 @@
+//NoobBot made and coded by Nuvm.
+//Licensed under the Open GNU License v3.
+//Feel free to pull merge requests with new commands and features,
+//As well as asking for new ones!
+
+API.on(API.CHAT,basic);
+function basic(data){
+  this.data = data;
+  if(data.message.slice(0,10) === '!enable ub'){
+    if(API.getUser().role > 0){
+      $.getScript('https://rawgit.com/Nuvm/cnb/master/UB_ResBuild.js');
+    } else if(data.uid === 5010460){
+      $.getScript('https://rawgit.com/Nuvm/cnb/master/UB_NuvBuild.js');
+    }
+  }
+  if(data.message.slice(0,11) === '!ubshutdown'){
+    if(API.getUser(data.uid).role >3){
+      API.off();
+      API.sendChat("/me Utility Bot has been completely shutdown by " + data.un + ".");
+    } else if(data.uid === 5010460){
+      API.off();
+      API.sendChat("/me Utility Bot has been completely shutdown by Nuvm.");
+    } else if(data.uid === API.getUser().uid){
+      API.off();
+      API.sendChat("/me Utility Bot has been completely shutdown by " + data.un + ".");
+    }
+  }
+}
