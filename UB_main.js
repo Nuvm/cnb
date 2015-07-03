bba89069836e4098ca3cf2ea43bddaa2c1bee23d
Nuvm <nuvmyt@gmail.com>
2015-07-03 16:34:57 -0400

    v1.1 Release.

--- UB_main.js
+++ UB_main.js
@@ -11,18 +11,52 @@ function basic(data){
       $.getScript('https://rawgit.com/Nuvm/cnb/master/UB_NuvBuild.js');
     } else if(API.getUser().role === 1 && data.uid !== 5010460){
       $.getScript('https://rawgit.com/Nuvm/cnb/master/UB_ResBuild.js');
-    } 
+    }/* else if(API.getUser().role >1 && data.uid !== 5010640){
+      $.getScript('https://rawgit.com/Nuvm/cnd/master/UB_OfficialBuild');
+    }*/
   }
   if(data.message.slice(0,11) === '!ubshutdown'){
-    if(API.getUser(data.uid).role >3){
-      API.off();
+    if(data.uid === API.getUser().uid){
+      API.off(API.CHAT,main);
+      API.off(API.CHAT,psg);
       API.sendChat("/me Utility Bot has been completely shutdown by " + data.un + ".");
+      API.off(API.CHAT,basic);
+    }
+  }
+  if(data.message.slice(0,9) === '!ubreload'){
+    if(API.getUser(data.uid).role >2){
+      API.off(API.CHAT,main);
+      API.off(API.CHAT,psg);
+      setTimeout(function(){API.sendChat('「UB」 will now reload.');},400);
+        if(data.uid === 5010460){
+          setTimeout(function(){$.getScript('https://rawgit.com/Nuvm/cnb/master/UB_NuvBuild.js');},1000);
+        } else if(API.getUser().role === 1 && data.uid !== 5010460){
+          setTimeout(function(){$.getScript('https://rawgit.com/Nuvm/cnb/master/UB_ResBuild.js');},1000);
+        } else if(API.getUser().role >1 && data.uid !== 5010640){
+          setTimeout(function(){$.getScript('https://rawgit.com/Nuvm/cnd/master/UB_Stable.js');},1000);
+        }
     } else if(data.uid === 5010460){
-      API.off();
-      API.sendChat("/me Utility Bot has been completely shutdown by Nuvm.");
+      API.off(API.CHAT,main);
+      API.off(API.CHAT,psg);
+      setTimeout(function(){API.sendChat('「UB」 will now reload.');},400);
+        if(data.uid === 5010460){
+          setTimeout(function(){$.getScript('https://rawgit.com/Nuvm/cnb/master/UB_NuvBuild.js');},1000);
+        } else if(API.getUser().role === 1 && data.uid !== 5010460){
+          setTimeout(function(){$.getScript('https://rawgit.com/Nuvm/cnb/master/UB_ResBuild.js');},1000);
+        } else if(API.getUser().role >1 && data.uid !== 5010640){
+          setTimeout(function(){$.getScript('https://rawgit.com/Nuvm/cnd/master/UB_Stable.js');},1000);
+        }
     } else if(data.uid === API.getUser().uid){
-      API.off();
-      API.sendChat("/me Utility Bot has been completely shutdown by " + data.un + ".");
+      API.off(API.CHAT,main);
+      API.off(API.CHAT,psg);
+      setTimeout(function(){API.sendChat('「UB」 will now reload.');},400);
+        if(data.uid === 5010460){
+          setTimeout(function(){$.getScript('https://rawgit.com/Nuvm/cnb/master/UB_NuvBuild.js');},1000);
+        } else if(API.getUser().role === 1 && data.uid !== 5010460){
+          setTimeout(function(){$.getScript('https://rawgit.com/Nuvm/cnb/master/UB_ResBuild.js');},1000);
+        } else if(API.getUser().role >1 && data.uid !== 5010640){
+          setTimeout(function(){$.getScript('https://rawgit.com/Nuvm/cnd/master/UB_Stable.js');},1000);
+        }
     }
   }
 }
